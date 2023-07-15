ee02c64fa60e87de65b416f2e87b9f8b24e840ff
Ayo Omotosho <aomotosho4@gmail.com>
2023-07-15 18:59:53 -0500

    add desc css on project items

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,8 +1,25 @@
+import { useState } from "react";
 import "../styles/_projects.scss";
 import { BsBoxArrowUpRight } from "react-icons/bs";
 import { RiMore2Fill } from "react-icons/ri";
+import { AiOutlineGithub } from "react-icons/ai";
 
 export default function Projects() {
+  const [toggle, setToggle] = useState(false);
+  const [toggleChat, setToggleChat] = useState(false);
+  const toggleOverlay = () => {
+    setToggle((prev) => !prev);
+  };
+
+  const toggleChatOverlay = () => {
+    setToggleChat((prev) => !prev);
+  };
+
+  const bugDescription =
+    "A bug tracker app that allows users to create, edit, and delete bugs. Users can also assign bugs to other users and change the status of the bug. The app uses React, Typescript, Tailwind, Node, Express, and MongoDB.";
+
+  const chatDescription =
+    "A chat app that allows users to create an account, login, and chat with other users. The app uses React Native, Expo, and Firebase.";
   return (
     <div className="project-page" id="projects">
       <div className="project-width">
@@ -12,24 +29,45 @@ export default function Projects() {
 
         <div className="project-grid">
           {/*TODO: make these clickable */}
-          <div className="project-item">
-            <div className="project">
-              <div className="bug-tracker"></div>
-            </div>
 
+          <div className="project-item">
+            <div className="project ">
+              {toggle && (
+                <div className="overlay">
+                  <div>
+                    <p>{bugDescription}</p>
+                  </div>
+                </div>
+              )}
+
+              <div
+                className={`${toggle ? "noHoverBugTracker" : "bug-tracker"}`}
+              ></div>
+            </div>
             <div className="subtitle">
               <div className="subtitle-left">
                 <h3>Bug Tracker</h3>
                 <BsBoxArrowUpRight className="box" size={20} color="white" />
+                <AiOutlineGithub className="github" size={24} color="white" />
               </div>
-              <RiMore2Fill className="more" size={20} color="white" />
+              <RiMore2Fill
+                className="more"
+                size={20}
+                color="white"
+                onClick={toggleOverlay}
+              />
             </div>
           </div>
-          <div className="project-item">
-            <div className="project chat-app-imgs">
-              <div className="login"></div>
-              <div className="signup"></div>
-              <div className="chat"></div>
+          <div className="project-item ">
+            <div className="project ">
+              {toggleChat && (
+                <div className="overlay">
+                  <div>
+                    <p>{chatDescription}</p>
+                  </div>
+                </div>
+              )}
+              <div className={`${toggleChat ? "noHoverLogin" : "login"}`}></div>
             </div>
 
             <div className="subtitle">
@@ -37,17 +75,13 @@ export default function Projects() {
                 <h3>Chat App</h3>
                 <BsBoxArrowUpRight className="box" size={20} color="white" />
               </div>
-              <RiMore2Fill className="more" size={20} color="white" />
+              <RiMore2Fill
+                className="more"
+                size={20}
+                color="white"
+                onClick={toggleChatOverlay}
+              />
             </div>
-          </div>
-
-          <div className="project-item">
-            <div className="project"></div>
-            <h3></h3>
-          </div>
-          <div className="project-item">
-            <div className="project"></div>
-            <h3></h3>
           </div>
         </div>
       </div>
