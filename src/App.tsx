2bb8f0b7aa5ff00a61fff26fc5c224cfd3ee90c4
Ayo Omotosho <aomotosho4@gmail.com>
2023-09-11 17:26:06 -0500

    fix: profile img margin

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import './styles/App.scss'
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import About from './components/About'
-import Projects from './components/Projects'
+import "./styles/App.scss";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Projects from "./components/Projects";
 function App() {
-
-
   return (
     <>
       <Home />
       <Navbar />
       <About />
-      <Projects/>
+      <Projects />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
