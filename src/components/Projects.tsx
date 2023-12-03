9766c4a8154d1128721c395fd2e7da7d0ac844a3
Ayo Omotosho <aomotosho4@gmail.com>
2023-12-02 18:56:51 -0600

    add: add 2 projects and refactor project component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,23 +5,122 @@ import { RiMore2Fill } from "react-icons/ri";
 import { AiOutlineGithub, AiOutlineYoutube } from "react-icons/ai";
 import bugImage from "../assets/bug-tracker-img.png";
 import chatImage from "../assets/login.png";
+import typingImage from "../assets/geckotype.png";
+import discord from "../assets/discord.png";
 
 export default function Projects() {
   const [toggle, setToggle] = useState(false);
   const [toggleChat, setToggleChat] = useState(false);
-  const toggleOverlay = () => {
-    setToggle((prev) => !prev);
+
+  const [projectItems, setProjectItems] = useState([
+    {
+      index: 0,
+      show: false,
+      title: "Bug Tracker",
+      description:
+        "A bug tracker app that allows users to create, edit, and delete bugs. Users can also assign bugs to other users and change the status of the bug. The app uses React, Typescript, Tailwind, Node, Express, and MongoDB.",
+      link: "https://bug-frontend-oj63.onrender.com",
+      youtube: "https://youtu.be/5pqGUTb2sDg",
+      github: "https://github.com/Ayomide4/bug-tracker",
+      image: bugImage,
+    },
+
+    {
+      index: 1,
+      show: false,
+      title: "geckotype",
+      description:
+        "a speed typing website that allows you to track your stats over time.",
+      link: "https://ayomide4.github.io/geckotype/",
+      youtube: "",
+      github: "https://github.com/Ayomide4/geckotype",
+      image: typingImage,
+    },
+
+    {
+      index: 2,
+      show: false,
+      title: "Discord Clone",
+      description:
+        "Full stack discord clone with voice/video chat using React, TypeScript, Vite and spring boot",
+      link: "",
+      github: "https://github.com/Ayomide4/discord-clone",
+      youtube: "",
+      image: discord,
+    },
+  ]);
+
+  console.log(projectItems);
+
+  const toggleOverlay = (projectIndex: number) => {
+    setProjectItems((prevItems) => {
+      return prevItems.map((prevItem) => {
+        if (prevItem.index === projectIndex) {
+          return {
+            ...prevItem,
+            show: !prevItem.show,
+          };
+        }
+        return prevItem;
+      });
+    });
   };
 
   const toggleChatOverlay = () => {
     setToggleChat((prev) => !prev);
   };
 
-  const bugDescription =
-    "A bug tracker app that allows users to create, edit, and delete bugs. Users can also assign bugs to other users and change the status of the bug. The app uses React, Typescript, Tailwind, Node, Express, and MongoDB.";
-
   const chatDescription =
     "A chat app that allows users to create an account, login, and chat with other users. The app uses React Native, Expo, and Firebase.";
+
+  const projectItem = projectItems.map((item, projectIndex) => {
+    return (
+      <div className="project-item" key={projectIndex}>
+        <div className="project ">
+          {item.index === projectIndex && item.show ? (
+            <div className="overlay">
+              <div>
+                <p>{item.description}</p>
+              </div>
+            </div>
+          ) : null}
+
+          {/*toggle && (
+            <div className="overlay">
+              <div>
+                <p>{item.description}</p>
+              </div>
+            </div>
+          )*/}
+
+          <div className={`${toggle ? "noHoverBugTracker" : "bug-tracker"}`}>
+            <img src={item.image} alt="bug-tracker" />
+          </div>
+        </div>
+        <div className="subtitle">
+          <div className="subtitle-left">
+            <a href={item.link}>
+              <h3>{item.title}</h3>
+              <BsBoxArrowUpRight className="box" size={20} color="white" />
+            </a>
+            <a href={item.github}>
+              <AiOutlineGithub className="github" size={24} color="white" />
+            </a>
+            <a href={item.youtube}>
+              <AiOutlineYoutube className="youtube" size={24} color="white" />
+            </a>
+          </div>
+          <RiMore2Fill
+            className="more"
+            size={20}
+            color="white"
+            onClick={() => toggleOverlay(projectIndex, item)}
+          />
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div className="project-page" id="projects">
       <div className="project-width">
@@ -30,47 +129,7 @@ export default function Projects() {
         </h1>
 
         <div className="project-grid">
-          <div className="project-item">
-            <div className="project ">
-              {toggle && (
-                <div className="overlay">
-                  <div>
-                    <p>{bugDescription}</p>
-                  </div>
-                </div>
-              )}
-
-              <div
-                className={`${toggle ? "noHoverBugTracker" : "bug-tracker"}`}
-              >
-                <img src={bugImage} alt="bug-tracker" />
-              </div>
-            </div>
-            <div className="subtitle">
-              <div className="subtitle-left">
-                <a href="https://bug-frontend-oj63.onrender.com">
-                  <h3>Bug Tracker</h3>
-                  <BsBoxArrowUpRight className="box" size={20} color="white" />
-                </a>
-                <a href="https://github.com/Ayomide4/bug-tracker">
-                  <AiOutlineGithub className="github" size={24} color="white" />
-                </a>
-                <a href="https://youtu.be/5pqGUTb2sDg">
-                  <AiOutlineYoutube
-                    className="youtube"
-                    size={24}
-                    color="white"
-                  />
-                </a>
-              </div>
-              <RiMore2Fill
-                className="more"
-                size={20}
-                color="white"
-                onClick={toggleOverlay}
-              />
-            </div>
-          </div>
+          {projectItem}
           <div className="project-item ">
             <div className="project ">
               {toggleChat && (
