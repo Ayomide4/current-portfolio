774a1e967b4169ab474cf75eafb913b1e281ce54
Ayo Omotosho <aomotosho4@gmail.com>
2023-12-08 17:23:38 -0600

    fix: remove /

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/https://ayomide4.github.io/current-portfolio/",
+  base: "https://ayomide4.github.io/current-portfolio/",
   plugins: [react()],
 });
